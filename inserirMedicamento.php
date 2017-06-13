d3d0b9f1468a11e184ec734795608736108899bb
TE21995 <thomasew123@gmail.com>
2017-06-12 21:28:16 -0300

    Create inserirMedicamento.php

--- inserirMedicamento.php
+++ inserirMedicamento.php
@@ -21,9 +21,25 @@ $resultado2 = mysqli_query($conn, $query2);
 
 <head>
 <meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="main.css" type="text/css"> 
 </head>
 
-<body>
+<body class="bg-faded">
+  <div class="p-1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center">infoMedicine</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 bg-faded">
 <form name="Fabricante" method="post" action="cadastrarMedicamento.php">
 
 Nome: <input type="text" name="nome"><br/></br>
@@ -52,3 +68,23 @@ Principio: <select name="principio">
 </select></br></br>
 <input type="submit" name="Enviar">
 </form>
+</br></br>
+          <a href="inserirPrincipio.php" class="btn btn-primary" style="float: right;">Inserir Principio Ativo</a>
+          <a href="inserirFabricante.php" class="btn btn-outline-primary">Inserir Fabricante</a>          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-faded">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"><a href="index.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Pesquisa básica</a></div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+  <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+</body>
+
+</html>
