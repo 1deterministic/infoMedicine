7d9d3effa9d5f2135c2c44803d499fcd19ad8417
TE21995 <thomasew123@gmail.com>
2017-06-13 12:58:10 -0300

    Create inserirMedicamento.php
    Simplificar o código.

--- inserirMedicamento.php
+++ inserirMedicamento.php
@@ -46,15 +46,8 @@ Nome: <input type="text" name="nome"><br/></br>
 Imagem_URL: <input type="URL" name="url"></br></br>
 Codigo de Registro: <input type="text" name="registro"></br></br>
 Referência: <input type="text" name="referencia"></br></br>
-Apresentação: <input type="text" name="apresentacao"><br/></br>
-Precauções: <input type="text" name="precaucoes"></br></br>
-Esquemas de Administração: <input type="text" name="esquemas"><br/></br>
-Aspectos Farmacologicos: <input type="text" name="aspectos"><br/></br>
-Efeitos Adversos: <input type="text" name="efeitos"><br/></br>
-Interações Medicamentosas: <input type="text" name="interacoes"><br/></br>
-Orientações aos Pacientes: <input type="text" name="orientacoes"><br/></br>
-Aspectos Farmacêuticos: <input type="text" name="farma"><br/></br>
-Advertências: <input type="text" name="advertencias"><br/></br>
+<textarea name="descricao" rows="10" cols="100">
+</textarea></br></br>
 Fabricante: <select type="text" name="fabricante">
 <?php while($row1 = mysqli_fetch_array($resultado1)):;?>
 <option><?php echo $row1[0];?></option>
