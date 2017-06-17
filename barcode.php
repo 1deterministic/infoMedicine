f1ace7100e206771f772202757180d4169d28ad2
TE21995 <thomasew123@gmail.com>
2017-06-17 18:30:39 -0300

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -3,11 +3,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="main.css" type="text/css"> </head>
 
 <body class="bg-faded">
-  <div class="py-5">
+ <div class="py-5">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
@@ -20,10 +20,10 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 bg-faded">
-          <form class="" method="POST" action="barcode.php">
-            <div class="form-group"> <label></label> <input type="text" class="form-control" placeholder="Insira o Número de Registro aqui" name="barcode"> </div>
+          <form class="" method="GET" action="details.php">
+            <div class="form-group"> <label></label> <input type="text" class="form-control" placeholder="Insira o código de barras aqui" type="text" name="barcode" id="barcode"> </div>
             <input type="submit" class="btn btn-primary" style="float: right;">
-          </form>
+         </form>
         </div>
       </div>
     </div>
@@ -31,8 +31,8 @@
   <div class="py-5 bg-faded">
     <div class="container">
       <div class="row">
-        <div class="col-md-12"><a href="index.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Pesquisa básica</a><a href="advanced.php" class="btn btn-block btn-primary btn-sm my-5" data-toggle="">Pesquisa avançada</a></div>
-      </div>
+       <div class="col-md-12"><a href="index.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Pesquisa básica</a><a href="advanced.php" class="btn btn-block btn-primary btn-sm my-5" data-toggle="">Pesquisa avançada</a></div>
+     </div>
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
@@ -40,25 +40,4 @@
   <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
 </body>
 
-<?php
-
-include 'conection.php';
-
-if (isset($_POST['barcode'])) {
-$codig=$_POST['barcode'];
-
-$cod_query = "SELECT * FROM `medicamento` WHERE Codigo_de_Registro='$codig'";
-
-$resultcod = mysqli_query($conn,$cod_query);
-while ($rowc = $resultcod->fetch_assoc()) {
-	$bar = $rowc['Nome'];
-	header("refresh:0; url='advancedDetails.php?nome=$bar'");
-}
-if(mysqli_num_rows($resultcod) == 0) {
-  echo "<h2>NENHUM RESULTADO ENCONTRADO</h2>";
-  header("refresh:3; url=barcode.php");
-}
-}
-?>
-
-</html>
+</html> 
