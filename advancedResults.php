b774d1326ced25ecbc32f3b291c51bdbd4f0eda9
TE21995 <thomasew123@gmail.com>
2017-06-17 16:09:06 -0300

    Update advancedResults.php

--- advancedResults.php
+++ advancedResults.php
@@ -1,19 +1,6 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$dbname="infomedicine";
-$conn= mysqli_connect($host,$user,$pass,$dbname);
-if(!$conn)
-{
-	echo "A conexão falhou";
-}
-else{
-	echo "A conexão foi bem succedida</br></br>";
-}
-
-mysqli_set_charset($conn,"utf8");
+include 'conection.php';
 
 $nome=$_POST['nome'];
 $fabricante=$_POST['fabricante'];
