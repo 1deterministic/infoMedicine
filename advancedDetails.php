349cc11ee7aa9ba0c2e73ad1b456935ab5b768c1
TE21995 <thomasew123@gmail.com>
2017-06-17 16:11:37 -0300

    Update advancedDetails.php

--- advancedDetails.php
+++ advancedDetails.php
@@ -1,19 +1,6 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$dbname="infomedicine";
-$conn= mysqli_connect($host,$user,$pass,$dbname);
-if(!$conn)
-{
-	echo "A conexão falhou";
-}
-else{
-	echo "A conexão foi bem succedida</br></br>";
-}
-
-mysqli_set_charset($conn,"utf8");
+include 'conection.php';
 
 $myname=$_GET['nome'];
 
@@ -26,19 +13,19 @@ $result2 = mysqli_query($conn,$d_query);
    $img = $row['Imagem_URL'];
    echo "<img src='$img' width=150 height=150 />"."<hr>";
    echo "Numero de Registro - MS: ".$row['Codigo_de_Registro']."</br><hr>";
-   echo "Medicamento de Referencia: ".$row['Referencia']."</br><hr>";
-   echo "<h3>Descrição: </h3></br>".$row['Descricao']."</br><hr>";
+   echo "Medicamento de Referencia: ".$row['Referencia']."</br><hr>";  
+   echo "<h3>Descrição:</h3> ".nl2br($row['Descricao']);
+   
    $fabN = $row['Fabricante'];
    $fab_query = "SELECT * FROM `fabricante` WHERE Nome='$fabN'";
    $resultf = mysqli_query($conn,$fab_query);
    while ($rowf = $resultf->fetch_assoc()) {
-	   echo "Fabricante: </br></br><h2>".$rowf['Nome']."</h2>"."<br>";
+	   echo "<hr><h3>Fabricante:</h3> <h2>".$rowf['Nome']."</h2>"."<br>";
 	   $imgf = $rowf['Imagem_URL'];
 	   echo "<img src='$imgf' width=300 height=150 />"."</br></br>";
-	   echo "<h3>Descrição:</h3> ".$rowf['Descricao']."<br>";
-	   echo "</br><h3>Informações de Contato:</h3> ".$rowf['Contato']."<br>";
-	  //echo nl2br($rowf['Contato']);
-	   echo "</br><h3>Endereço:</h3> ".$rowf['Endereco']."<br>";
+       echo "</br><h3>Descrição: :</h3> ".nl2br($rowf['Descricao'])."<br>";
+	   echo "<hr><h3>Informações de Contato: </h3>".nl2br($rowf['Contato']);
+	   echo "<hr></br><h3>Endereço:</h3> ".$rowf['Endereco']."<br>";
    }
    echo "</br><hr></br>";
    
@@ -46,8 +33,8 @@ $result2 = mysqli_query($conn,$d_query);
    $pm_query = "SELECT * FROM `principio_ativo` WHERE Nome='$pname'";
    $resultp = mysqli_query($conn,$pm_query);
    while ($rowp = $resultp->fetch_assoc()) {
-	   echo "Principio Ativo: <h2>".$rowp['Nome']."</h2><br>";
-	   echo "<h3>Descrição:</h3> </br>".$rowp['Descricao']."</br></br>"; 
+	   echo "<h3>Principio Ativo:</h3> <h2>".$rowp['Nome']."</h2><br>";
+	   echo "<h3>Descrição:</h3> </br>".nl2br($rowp['Descricao'])."</br></br>"; 
    }
    echo "</br><hr>";
  }
