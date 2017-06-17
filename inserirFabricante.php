d1bb12c160565b6609208d0e384344ace67fe14a
TE21995 <thomasew123@gmail.com>
2017-06-16 21:01:04 -0300

    Update inserirFabricante.php

--- inserirFabricante.php
+++ inserirFabricante.php
@@ -23,12 +23,12 @@
         <div class="col-md-12 bg-faded">
 <form name="Fabricante" method="post" action="cadastrarFabricante.php">
 
-Nome: <input type="text" name="nome" placeholder="Nome do Fabricante" size="125"><br/></br>
-URL da Imagem: <input type="URL" name="url" placeholder="URL da Imagem" size="125"></br></br>
-Descricao: <textarea name="descricao" placeholder="Descrição do Fabricante" rows="10" cols="125">
+Nome:</br> <input type="text" name="nome" placeholder="Nome do Fabricante"><br/></br>
+URL da Imagem:</br> <input type="URL" name="url" placeholder="URL da Imagem"></br></br>
+Descricao:</br> <textarea name="descricao" placeholder="Descrição do Fabricante">
 </textarea></br></br>
-Contato: <input type="text" name="contato" placeholder="Contato do Fabricante: Telefone, Email, etc..." size="125"></br></br>
-Endereco: <input type="text" name="endereco" placeholder="Endereço do Fabricante" size="125"></br></br>
+Contato:</br> <input type="text" name="contato" placeholder="Telefone, Email, etc..."></br></br>
+Endereco:</br> <input type="text" name="endereco" placeholder="Endereço do Fabricante"></br></br>
  <input class="btn btn-primary" type="submit" name="Enviar" style="float: left;">
 </form>
 </br></br>
