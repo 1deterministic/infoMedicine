cf56512c1400e11fc596fb997b34a82aac298c1c
TE21995 <thomasew123@gmail.com>
2017-06-19 16:01:56 -0300

    Update alterar.php

--- alterar.php
+++ alterar.php
@@ -1,3 +1,32 @@
+<html>
+
+<head>
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="main.css" type="text/css"> 
+</head>
+
+<body class="bg-faded">
+  <div class="p-1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center">infoMedicine</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+      <div class="py-5 bg-faded">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"><a href="display.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Voltar</a></div>
+      </div>
+    </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+
 <?php
 //Pagina de demonstração realizando todas as operações na mesma pagina
 //Funciona mas fica um tanto desorganizado
@@ -182,3 +211,16 @@ $sql = "UPDATE `principio_ativo` SET Descricao='$descricao' WHERE Nome='$nome'";
 }
 
 ?>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+  <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+</body>
+
+</html>
