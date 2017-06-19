efc5f526bb1b06e03c950df590a8d3c52a15306f
TE21995 <thomasew123@gmail.com>
2017-06-19 16:05:03 -0300

    Update inserirFabricante.php

--- inserirFabricante.php
+++ inserirFabricante.php
@@ -43,7 +43,7 @@ Endereco:</br> <textarea name="endereco" placeholder="Endereço do Fabricante">
   <div class="py-5 bg-faded">
     <div class="container">
       <div class="row">
-        <div class="col-md-12"><a href="index.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Pesquisa básica</a></div>
+        <div class="col-md-12"><a href="crud.php" class="btn btn-block btn-primary btn-sm" data-toggle="">Voltar ao CRUD</a></div>
       </div>
     </div>
   </div>
